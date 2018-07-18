a7f8db4200a4aaa940f9c15084b23ab66a5dcebe
chenxin <361006796@qq.com>
2018-07-18 20:42:12 +0800

    aa

---
+++ src/main/java/bigdata/zklock/MyWatcher.java
@@ -0,0 +1,16 @@
+package bigdata.zklock;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+
+public class MyWatcher implements Watcher{
+
+	@Override
+	public void process(WatchedEvent event) {
+
+
+		
+		
+	}
+
+}
